39c3189449e1c1e8e5592d9d7bc375ecbea531a2
Maulik Shah <maulik.shah715@gmail.com>
2020-10-19 21:44:07 +0530

    Added some basic files

--- Game.py
+++ Game.py
@@ -4,6 +4,8 @@ pygame.init()
 screen = pygame.display.set_mode((576, 840))
 clock = pygame.time.Clock()
 
+bg_surface = pygame.image.load()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
